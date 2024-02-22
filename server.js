1a6afa20514cac240dc445db1340b4f5c627b3ad
= <s3tha@icloud.com>
2024-02-22 07:08:33 -0600

    finished all the features

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
     user: 'root',
-    // TODO: Add MySQL password here
     password: '',
     database: 'employee_db'
   },
@@ -26,8 +24,6 @@ function SQLDisconnect() {
     });
 
 }
-
-// Grab the initial databases, probably not the most efficent way to do it but hey, it works
 
 let employees
 let roles
@@ -101,8 +97,6 @@ function UpdateDB() {
 
 }
 
-UpdateDB()
-
 // All the menu functions, to AT THE VERY LEAST not have a bunch of if statements ~~my god it's still a fucking mess though I am so sorry whoever is reviewing this~~
 
 const actions = {
@@ -175,7 +169,7 @@ const actions = {
             db.query(
                 `
                 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-                VALUES (${firstName},${lastName},${roleID},${roleID});
+                VALUES ("${firstName}","${lastName}",${roleID},${managerID});
                 `
                 , (err, rows) => {
                 if (err) {
@@ -192,6 +186,68 @@ const actions = {
 
     "Update Employee Role": function() {
 
+        const roleNames = []
+        const employeesLocalTable = []
+
+        for (let i = 0; i < roles.length; i++) {
+            roleNames.push(roles[i].title)
+        } 
+
+        for (let i = 0; i < employees.length; i++) {
+            employeesLocalTable.push(`${employees[i].first_name} ${employees[i].last_name}`)
+        } 
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'employeeName',
+                message: `Who is the employee?`,
+                choices: employeesLocalTable
+            },
+            {
+                type: 'list',
+                name: 'roleName',
+                message: `What is the new role?`,
+                choices: roleNames
+            },
+        ])
+        .then(({ employeeName, roleName,}) => {
+
+            var roleID
+            var employeeID
+
+            console.log(employeeName)
+
+            for (let i = 0; i < employees.length; i++) {
+                const currEmployee = employees[i]
+                if (employeeName == `${currEmployee.first_name} ${currEmployee.last_name}`) {
+                    employeeID = employees[i].id
+                }
+            }
+
+            for (let i = 0; i < roles.length; i++) {
+                if (roles[i].title == roleName) {
+                    roleID = roles[i].id
+                }
+            }
+
+            db.query(
+                `
+                UPDATE employee
+                SET role_id = ${roleID}
+                WHERE id = ${employeeID}
+                `
+                , (err, rows) => {
+                if (err) {
+                    console.log(`ERROR WHILE UPDATING EMPLOYEE: ${err.message}`)
+                   return;
+                }
+            });
+            console.log(`Updated ${employeeName}'s role!`)
+            main()
+    
+        })
+
     },
 
     "View All Roles": function() {
@@ -203,6 +259,64 @@ const actions = {
     },
 
     "Add Role": function() {
+
+        const departmentNames = []
+
+        for (let i = 0; i < departments.length; i++) {
+            departmentNames.push(departments[i].name)
+        }
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'roleName',
+                message: `What is the role's name?`,
+            },
+            {
+                type: 'list',
+                name: 'roleDepartment',
+                message: `What department is it in??`,
+                choices: departmentNames
+            },
+            {
+                type: 'input',
+                name: 'roleSalary',
+                message: `What is the role's salary?`,
+            },
+        ])
+        .then(({ roleName, roleDepartment, roleSalary}) => {
+
+            var departmentID
+
+            for (let i = 0; i < departments.length; i++) {
+                if (departments[i].name == roleDepartment) {
+                    departmentID = departments[i].id
+                }
+            }
+
+            if (Number(roleSalary)) {
+
+                db.query(
+                    `
+                    INSERT INTO role(title, salary, department_id)
+                    VALUES ("${roleName}",${Number(roleSalary)},${departmentID});
+                    `
+                    , (err, rows) => {
+                    if (err) {
+                        console.log(`ERROR WHILE ADDING ROLE: ${err.message}`)
+                       return;
+                    }
+                });
+                console.log(`Added the role ${roleName}!`)
+
+            } else {
+                console.log("Please input a number for the salary.")
+            }
+
+            main()
+    
+        })
+
 
     },
 
@@ -216,6 +330,32 @@ const actions = {
 
     "Add Department": function() {
 
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'departmentName',
+                message: `What is the department's name?`,
+            },
+        ])
+        .then(({ departmentName}) => {
+
+            db.query(
+                `
+                INSERT INTO department (name)
+                VALUES ("${departmentName}");
+                `
+                , (err, rows) => {
+                if (err) {
+                    console.log(`ERROR WHILE ADDING DEPARTMENT: ${err.message}`)
+                   return;
+                }
+            });
+            console.log(`Added the department ${departmentName}!`)
+
+            main()
+    
+        })
+
     },
 
     "Quit": function() {
@@ -226,7 +366,11 @@ const actions = {
     },
 }
 
+// The actual menu
+
 const main = function() {
+
+    UpdateDB()
 
     inquirer.prompt([
         {
@@ -237,8 +381,6 @@ const main = function() {
         },
     ])
     .then(({ action }) => {
-
-        UpdateDB()
 
         if (actions[action]) {
             actions[action]()
